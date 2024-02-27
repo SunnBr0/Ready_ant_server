0a575505219d5bb66cbef720cb5a157481c50d2e
Voronov Vladimir <vova.voronov.2002@bk.ru>
2024-02-28 00:21:28 +0300

    trying to make a board

--- static/script/draw_redactor/redactor.js
+++ static/script/draw_redactor/redactor.js
@@ -6,70 +6,69 @@ let draw_instruments = document.getElementById("draw_instruments")
 let context = draw_map.getContext('2d');
 console.dir(draw_instruments);
 
-let delta_x = -context.canvas.offsetLeft
-let delta_y = -context.canvas.offsetTop
-let Tools_draw = new ToolsDraw(context, delta_x, delta_y)
-let choice = null
+let Tools_draw = new ToolsDraw(context)
 Tools_draw.drawPoint(20, 30, 'A', 'red', 1);
 
+let choice = null
 let is_drawing = false
-let draw_width = "2"
-let draw_color = "black"
+let draw_width = 5
+let draw_color = "red"
 
 
 
 
-function start(event){
+function start(event,draw_color,draw_width){
     is_drawing = true
     context.beginPath()
-    context.moveTo(event.clientX + delta_x,event.clientY + delta_y)
-    context.lineTo(event.clientX + delta_x,event.clientY + delta_y)
+    context.strokeStyle = draw_color
+    context.lineWidth = draw_width
+    context.lineTo(event.offsetX,event.offsetY)
+    context.stroke()
+    console.log("d");
+    // context.moveTo(event.clientX + delta_x,event.clientY + delta_y)
+    // context.lineTo(event.clientX + delta_x,event.clientY + delta_y)
 }
-function draw(event,color = draw_color,size = draw_width,obj = "dada"){
-    if(is_drawing){
-        context.lineTo(event.clientX + delta_x,event.clientY + delta_y)
-        context.strokeStyle = color
-        context.lineWidth = size
-        context.lineCap = "round"
-        context.lineJoin = "round"
-        context.stroke()
-        console.log(obj);
-    }
-
-}
-function stop(event){
-    if(is_drawing){
-        context.stroke()
-        context.closePath()
-        is_drawing = false
-    }
-    
-
-}
-draw_instruments.addEventListener("click", (event) => {
-    choice = event.target.closest("button").value
+function draw(event,color = draw_color,size = draw_width,choice){
+    if(!is_drawing) return
+    // context.lineTo(event.clientX + delta_x,event.clientY + delta_y)
     switch (choice) {
-        case "clear":
-            draw_map.addEventListener("mousedown",start,false)
-            draw_map.addEventListener("mousemove",(event)=>draw(event,"red",2,"aaaddd"),false)
-            draw_map.addEventListener("mouseup",stop,false)
-            draw_map.addEventListener("mouseout",stop,false)
-            console.dir("Dasd");
-            console.dir(draw_map.addEventListener);
-            break;
         case "draw":
-
-            // draw_map.addEventListener('click', (event) => draw_point_linear(event, 0))
-            // draw_map.addEventListener('mousemove', (event) => draw_point_linear(event, 1))
-            draw_map.addEventListener("mousedown",start,false)
-            draw_map.addEventListener("mousemove",(event)=>draw(event),false)
-            draw_map.addEventListener("mouseup",stop,false)
-            draw_map.addEventListener("mouseout",stop,false)
-            console.log("f");
+            context.lineTo(event.offsetX,event.offsetY)
+            context.stroke()
+            break;
+        case "clear":
+            context.strokeStyle = "#fff"
+            context.lineTo(event.offsetX,event.offsetY)
+            context.stroke()
             break;
         default:
             break;
     }
+    // context.lineCap = "round"
+    // context.lineJoin = "round"
+    console.log("obj");
+    console.log(event.offsetX);
+    console.log(event.offsetY);
+}
+function stop(event){
+    // if(is_drawing){
+    //     context.stroke()
+    //     context.closePath()
+    //     is_drawing = false
+    // }
+        is_drawing = false
+    
+
+}
+
+draw_map.addEventListener("mousedown",(event)=>start(event,draw_color,draw_width),false)
+draw_map.addEventListener("mousemove",(event)=>draw(event,draw_color,draw_width,choice),false)
+draw_map.addEventListener("mouseup",stop,false)
+draw_map.addEventListener("mouseout",stop,false)
+
+draw_instruments.addEventListener("click", (event) => {
+    choice = event.target.closest("button").value
+    console.log(choice);
 })
 
 
