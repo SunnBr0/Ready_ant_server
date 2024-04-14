8f053301c4e5208750d6c359cfed48279cf1359c
Vladimir <vova.voronov.2002@bk.ru>
2024-04-14 11:37:40 +0300

    rust dataAnt straight on js server

--- Ready_ant_server/generation_rs.js
+++ Ready_ant_server/generation_rs.js
@@ -1,20 +1,67 @@
 import net from 'net'
 import fs from 'fs'
 import{full_path,dir_path_folder_test} from "./server/path_name_dir.mjs"
-// console.log(client.readyState);
-setInterval(()=>{
-let client = net.connect({ port: 6142,family:4 });
-    client.on('data', function (data) {
-        fs.writeFile(full_path("/ant_basic/ant_rs.json"),data,(err)=>{})
-        // console.log(data.toString());
+// // console.log(client.readyState);
+// let onServerDataAnt = false
+
+// function connectRustDataOnServerJs(onServerDataAnt){
+//     let obj ={}
+//     setInterval(()=>{
+//     let client = net.connect({ port: 6142,family:4 });
+//         client.on('data', function (data) {
+//             if(!onServerDataAnt){
+//                 fs.writeFile(full_path("/ant_basic/ant_rs.json"),data,(err)=>{})
+//                 console.log("work set json on file");
+//             }else{
+//                 obj =JSON.parse(data.toString())
+//                 console.log("work connect to js server");
+
+//             }
+    
+//         });
+//         client.on('connect', function () {
+//             console.log('Connected with Rust ');
+//         });
+//         client.on('error', function (err) {
+//             console.error("RUST engine data has been cut off");
+//         });
+//         client.on('close', function() {
+//             console.log('Connection closed with Rust');
+//         });
+//     },1000)
+// }
+
+// connectRustDataOnServerJs(onServerDataAnt)
+
+function straightRustDataOnJs(io,channel, onServerDataAnt) {
+let clientRust = net.connect({ port: 6142, family: 4 });
+
+    let obj = {}
+    clientRust.on('data', function (data) {
+      if (!onServerDataAnt) {
+        // console.log("work set json on file");
+        fs.writeFile(full_path("/ant_basic/ant_rs.json"), data, (err) => { })
+      } else {
+        try {
+          obj = JSON.parse(data.toString())
+            // console.log("work connect to js server json process on Server");
+            io.room(channel.roomId).emit('chat message', obj)
+        }
+        catch { }
+      }
+  
     });
-    client.on('connect', function () {
-        console.log('Connected with Rust ');
+    clientRust.on('connect', function () {
+      console.log('Connected with Rust ');
     });
-    client.on('error', function (err) {
-        console.error("RUST engine data has been cut off");
+    clientRust.on('error', function (err) {
+      console.error("RUST engine data has been cut off");
     });
-    client.on('close', function() {
-        console.log('Connection closed with Rust');
+    clientRust.on('close', function () {
+      console.log('Connection closed with Rust');
     });
-},1000)
+  }
+
+  export{
+    straightRustDataOnJs
+  }
--- Ready_ant_server/static/script/client.js
+++ Ready_ant_server/static/script/client.js
@@ -1,22 +1,25 @@
 // import{drawAnt} from "./draw_cansvas/draw.js"
-import{drawAnt} from "../script/draw_cansvas/draw.js"
+import { drawAnt } from "../script/draw_cansvas/draw.js"
 
-const channel = geckos({ port: 3000 }) 
+const channel = geckos({ port: 3000 })
 
 
 channel.onConnect(error => {
   if (error) {
     return error.message
   }
- channel.on('chat message', data => {
-    try{
-      drawAnt(JSON.parse(data))
-    }catch{
+  channel.on('chat message', data => {
+
+    try {
+      //присылается сразу json и его распарсиваем
+      drawAnt(data)
+
+    } catch {
     }
   })
-  
+
 })
 
-channel.onDisconnect(err=>{
+channel.onDisconnect(err => {
   console.log("Stop_Erver");
 })
